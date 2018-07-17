ecbdd9969b9116a7ff9355775e1ce4ee3d6e73d9
tacomother <tacomother99@gmail.com>
2018-07-17 14:58:46 +0900

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
-<?php echo "Hello World"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LOGIN SYSTEM</title>
+
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.9/css/uikit.min.css" /> 
+</head>
+<body>
+
+  <div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-l-3@s uk-child-width-l-l" uk-grid="">
+      <form class="uk-form-stacked">
+
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Email</label>
+          <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@example.com">
+          </div>
+        </div>
+
+        <div class="uk-margin">
+          <label class="uk-form-label" for="form-stacked-text">Password</label>
+          <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password">
+          </div>
+        </div>
+
+        <div class="uk-margin">
+          <button class="uk-button uk-button-default" type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  
+   <!-- UIkit JS -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.9/js/uikit.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.9/js/uikit-icons.min.js"></script>
+</body>
+</html>
